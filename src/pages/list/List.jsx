0e4d741d99cdb2c5a8084d91eec1df162c915e08
Md- Hasmat <mdhasmat690>
2022-09-26 10:46:48 +0600

    first commit

---
+++ src/pages/list/List.jsx
@@ -0,0 +1,16 @@
+import "./list.scss"
+
+
+const List = () => {
+  return (
+    <div className="list">
+      {/* <Sidebar/> */}
+      <div className="listContainer">
+        {/* <Navbar/> */}
+        {/* <Datatable/> */}
+      </div>
+    </div>
+  )
+}
+
+export default List
